a2169dcebd615e00c857cb18ae319c3f663ce5a2
Zach Aysan <zachaysan@gmail.com>
2014-02-10 13:30:25 -0500

    Create html to fix stop stuff

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-$(function(){
-
-	stop();	
-
-});		
-
-
-var stop = function(){
-	var testfile = $('.test')[0];
-	testfile.pause(function(){
-		console.log(testfile.currentTime);
-	});
-	// $('.test')[0].currentTime=0;
-}
+$("#stopBtn").click(function () {
+    console.log("stopping!");
+    var audio = document.getElementsByTagName('audio')[0];
+    audio.pause();
+    audio.currentTime = 0;
+});
