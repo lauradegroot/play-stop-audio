d4f20425bee37b68fcf16969b2103ef6810fd44d
Zach Aysan <zachaysan@gmail.com>
2014-02-10 13:52:09 -0500

    Fixed

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-$("#stopBtn").click(function () {
+$("#audio").bind('pause', function (e) {
+    console.log(e)
+    
     console.log("stopping!");
     var audio = document.getElementsByTagName('audio')[0];
     audio.pause();
     audio.currentTime = 0;
+    e.preventDefualt
 });
