a090cb2babb7d648055164441e007faca21030b3
Laura DeGroot <laurardegroot@gmail.com>
2014-02-10 13:57:05 -0500

    tidied it up

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
+var audio = document.getElementsByTagName('audio')[0];
+
 $("#stopBtn").click(function () {
-    console.log("stopping!");
-    var audio = document.getElementsByTagName('audio')[0];
     audio.pause();
     audio.currentTime = 0;
     $("#stopBtn").hide();
     $("#playBtn").show();
 });
 
-$("#playBtn").click(function () {
-    console.log("stopping!");
-    var audio = document.getElementsByTagName('audio')[0];
-    audio.play();
-    $("#stopBtn").show();
+$("#playBtn").click(function () {    
+    audio.play();  
     $("#playBtn").hide();
+    $("#stopBtn").show();
 });
