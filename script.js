593c803d648f4826581431019805302b22657faa
Laura DeGroot <laurardegroot@gmail.com>
2014-02-10 13:52:55 -0500

    play button and stop button hiding and showing to make it seem like they're one button

--- script.js
+++ script.js
@@ -3,4 +3,14 @@ $("#stopBtn").click(function () {
     var audio = document.getElementsByTagName('audio')[0];
     audio.pause();
     audio.currentTime = 0;
+    $("#stopBtn").hide();
+    $("#playBtn").show();
+});
+
+$("#playBtn").click(function () {
+    console.log("stopping!");
+    var audio = document.getElementsByTagName('audio')[0];
+    audio.play();
+    $("#stopBtn").show();
+    $("#playBtn").hide();
 });
